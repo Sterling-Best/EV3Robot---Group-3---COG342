149b8c6c32631d2a1b811bd69bc16f08bdbfe4ea
Sterling <sterling.best@mail.utoronto.ca>
2018-02-27 02:39:05 -0500

    Ev3Global - __init__(), varialbes, and TODOS
    Added class varialbes, create early version of __init__(), and created TODOs for future methods.

--- src/Ev3Grid/Ev3Global.py
+++ src/Ev3Grid/Ev3Global.py
@@ -3,7 +3,7 @@ from src.Ev3Grid.Ev3Region import Ev3Region
 
 class Ev3Global:
     """
-        Contains all regions and coordinates during a robot's run.
+    Contains all regions and coordinates during a robot's run.
     """
 
     __globalCoordinate: list
@@ -14,3 +14,24 @@ class Ev3Global:
         """
 
         """
+        __globalCoordinate = []
+        __regionSize = 16
+
+    #TODO: Create add coordinate
+
+    #TODO: Create add region
+
+    #TODO: Create region search/return
+
+    #TODO: Create Export csv file
+
+    #TODO: Optional: Modify region size
+    #Goes through all regions modifying their borders, and reassigning coordinates
+    #This will bye tough
+
+    #TODO: Create __str__
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
