b4539f697fc73793e85360e8f80c4d0b51c2fc3e
Sterling <sterling@mysix.com>
2018-03-04 16:14:19 -0500

    CameraMovement.cs - Added Drag mouse and move for camera
    Added the ability in CameraMovement .cs in Ev3Visualizer that allows the camera to be moved by clicking on part of the screen and dragging it in a desired direction.

--- Ev3Visualizer/Assets/Scripts/CameraMovement.cs
+++ Ev3Visualizer/Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+
+        transform.Translate(move, Space.World);
+    }
+
     public void LateUpdate()
     {
         float xAxisValue = Input.GetAxis("Horizontal");
