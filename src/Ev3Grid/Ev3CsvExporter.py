68bf876ff35d317ff87e534b968f62e3bf34c88a
Sterling <sterling@mysix.com>
2018-03-04 15:23:39 -0500

    Ev3CvsExporter - added datetimefilename()
    Aded Ev3CvsExporter.datetimefilename() that will generate new file names for the date and time when I csv file is created.
    
    Made small adjustments to the default project scene for the Ev3Visualizer

--- src/Ev3Grid/Ev3CsvExporter.py
+++ src/Ev3Grid/Ev3CsvExporter.py
@@ -1,4 +1,6 @@
 import csv
+import time
+import datetime
 
 class Ev3CsvExporter:
     """
@@ -27,3 +29,24 @@ class Ev3CsvExporter:
             for coord in coordlist:
                 filewriter.writerow([str(coord.get_xcoordinate()), str(coord.get_ycoordinate())])
         csvfile.close()
+
+    def datetimefilename(self) -> str:
+        """
+        Generates a string based on computers current date and time to generate a unique filename.
+
+        Returns:
+            str: String containing date and time, to be used as file name.
+
+        Doctests:
+        >>> aEx = Ev3CsvExporter()
+        >>> ts = time.time()
+        >>> ds = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d-%H%M%S')
+        >>> ds == aEx.datetimefilename()
+        True
+        """
+        timestamp = time.time()
+        return datetime.datetime.fromtimestamp(timestamp).strftime('%Y%m%d-%H%M%S')
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
