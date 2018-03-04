6bd5aeafc6736fca0d89ed599ac61af6396bbf8c
Sebastian Hein <slhein492@gmail.com>
2018-03-04 11:52:35 -0500

    Removed the class wrapper and function wrappers.

--- simpleBots/Coward.py
+++ simpleBots/Coward.py
@@ -1,53 +1,36 @@
 #!/usr/bin/env python3
 from ev3dev.ev3 as ev3
 
-class Coward:
-    """
-    Coward is an algorithm based on Braitenberg's vehicle 2A.
-    It should 'become restless in the vicinity of sources
-    avoiding them, escaping until it safely reaches a place
-    where the influence of the source is scarcely felt.' 
-    (Braitenberg, 1987)
-    """
+"""
+Coward is an algorithm based on Braitenberg's vehicle 2A.
+It should 'become restless in the vicinity of sources
+avoiding them, escaping until it safely reaches a place
+where the influence of the source is scarcely felt.' 
+(Braitenberg, 1987)
+"""
     
-    leftMotor: ev3.LargeMotor
-    rightMotor: ev3.LargeMotor
+self.leftMotor = ev3.LargeMotor('outA')
+self.rightMotor = ev3.LargeMotor('outD')
+self.leftSensor = ev3.ColorSensor('out1')
+self.rightSensor = ev3.ColorSensor('out4')
 
-    leftSensor: ev3.ColorSensor
-    rightSensor: ev3.ColorSensor
-
-
-    def __init__(self) -> None:
-        """
-        Initialize and start the program.
-        """
-        self.leftMotor = ev3.LargeMotor('outA')
-        self.rightMotor = ev3.LargeMotor('outD')
-        self.leftSensor = ev3.ColorSensor('out1')
-        self.rightSensor = ev3.ColorSensor('out4')
-        
-        self.leftMotor.run_forever(speed_sp = 0)
-        self.rightMotor.run_forever(speed_sp = 0)
-        self.leftSensor.mode = 'COL-AMBIENT'
-        self.rightSensor.mode = 'COL-AMBIENT'
+self.leftMotor.run_forever(speed_sp = 0)
+self.rightMotor.run_forever(speed_sp = 0)
+self.leftSensor.mode = 'COL-AMBIENT'
+self.rightSensor.mode = 'COL-AMBIENT'
 
 
-    def run() -> None:
-        """
-        Attach leftMotor to leftSensor and rightMotor to rightSensor.
-        Set the speed of each motor to the value of the intensity each
-        sensor is detecting.
-        Hit the back button to stop the program.
-        """
-        exitBtn = Button()
-        
-        while exitBtn.on_backspace() == False:
-            leftMotor.speed_sp = leftSensor.value()
-            rightMotor.speed_sp = rightSensor.value()
+"""
+Attach leftMotor to leftSensor and rightMotor to rightSensor.
+Set the speed of each motor to the value of the intensity each
+sensor is detecting.
+Hit the back button to stop the program.
+"""
+exitBtn = Button()
 
-        leftMotor.stop()
-        rightMotor.stop()
+while exitBtn.on_backspace() == False:
+    leftMotor.speed_sp = leftSensor.value()
+    rightMotor.speed_sp = rightSensor.value()
 
-if __name__ == "__main__":
-    run()
-            
+leftMotor.stop()
+rightMotor.stop()
