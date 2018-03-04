f71309d49ef52ff455381a9f9468b7216be7eb88
Sterling <sterling@mysix.com>
2018-03-04 12:16:49 -0500

    Ev3Visualiser - Initial Implmentation Compleete
    Completed initial inplementation of Ev3Visualiser in unity. Added CSVReader C# script, created DataPlotter C# script for generating points in the visual space. Added camera movement script to navigate the area space.
    
    Ev3Visualizer will now read basic points of obstruction in a csv generated by python files. And display them.

---
+++ Ev3Visualizer/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public void LateUpdate()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float yAxisValue = Input.GetAxis("Vertical");
+        if (Camera.current != null)
+        {
+            Camera.current.transform.Translate(new Vector2(xAxisValue, yAxisValue));
+        }
+   
+    }
+
+}
---
+++ Ev3Visualizer/Assets/Scripts/DataPlotter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPlotter : MonoBehaviour {
+
+    public string inputfile;
+
+    private List<Dictionary<string, object>> pointList;
+
+    public int columnx = 0;
+    public int columny = 1;
+
+    public string xName;
+    public string yName;
+
+    public GameObject PointPrefab;
+
+	// Use this for initialization
+	void Start () {
+        pointList = CSVReader.Read(inputfile);
+        Debug.Log(pointList);
+        List<string> columnList = new List<string>(pointList[1].Keys);
+        Debug.Log("There are " + columnList.Count + " columns in CSV");
+        foreach (string key in columnList)
+            Debug.Log("Column name is " + key);
+        xName = columnList[columnx];
+        yName = columnList[columny];
+        for (var i=0; i < pointList.Count; i++)
+        {
+            float x = System.Convert.ToSingle(pointList[i][xName]);
+            float y = System.Convert.ToSingle(pointList[i][yName]);
+            Instantiate(PointPrefab, new Vector2(x, y), Quaternion.identity);
+        }
+
+
+	}
+	
+
+}
