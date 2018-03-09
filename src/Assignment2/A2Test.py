584132e09f91d4017e9e6187caaf2a63ef1ce4ae
Sterling <sterling@mysix.com>
2018-03-09 15:37:06 -0500

    A2Test.py - Cofnigure for test run
    Configured A2Test.py for test run, seing if motors function correctly.

--- src/Assignment2/A2Test.py
+++ src/Assignment2/A2Test.py
@@ -1,2 +1,19 @@
 from src.Assignment2.Robot import Robot
 
+robot = Robot(10)
+
+leftMotor = robot.getMotor('left')
+rightMotor = robot.getMotor('right')
+
+
+def run() -> None:
+    """
+    Attach leftMotor to leftSensor and rightMotor to rightSensor.
+    Set the speed of each motor to the value of the intensity * _amplify.
+    Hit the back button to stop the program.
+
+    Returns:
+        None: continously run the robot, and check for button press.
+    """
+    robot.speedUp(leftMotor, 10)
+    robot.speedUp(rightMotor, 10)
