b2e7179909fbea64fa8f774681631759181980d3
Sterling <sterling@mysix.com>
2018-03-13 15:05:00 -0400

    A2Test.py - Adjusted Sensor tolerance to 1000

--- src/Assignment2/A2Test.py
+++ src/Assignment2/A2Test.py
@@ -34,7 +34,7 @@ def run() -> None:
     """
     while True:
         btn.process()
-        if robot.getSensorValue("left") > 10:
+        if robot.getSensorValue("left") > 1000:
             globalGrid.addcoord(Ev3Coordinates(0,0,"Color_black"))
             robot.speedUp(leftMotor, 20)
             robot.speedUp(rightMotor, 20)
