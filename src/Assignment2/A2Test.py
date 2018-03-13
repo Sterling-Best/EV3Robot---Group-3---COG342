b10c54700c7dac4d927554b1cf6a5f190a1a2e9c
Sterling <sterling@mysix.com>
2018-03-13 15:14:00 -0400

    A2Test.py Try and detect Color

--- src/Assignment2/A2Test.py
+++ src/Assignment2/A2Test.py
@@ -11,9 +11,9 @@ robot = Robot(10)
 leftMotor = robot.getMotor('left')
 rightMotor = robot.getMotor('right')
 leftSensor = robot.cSensorRight
-leftSensor.mode = 'COL-COLOR_White'
+leftSensor.mode = 'COL-COLOR'
 rightSensor = robot.cSensorLeft
-rightSensor.mode = 'COL-COLOR_Black'
+rightSensor.mode = 'COL-COLOR'
 btn = robot.getButtons()
 
 
@@ -34,10 +34,12 @@ def run() -> None:
     """
     while True:
         btn.process()
-        if robot.getSensorValue("left") > 50:
+        if rightSensor.value(1) > 50:
             globalGrid.addcoord(Ev3Coordinates(0,0,"Color_black"))
             robot.speedUp(leftMotor, 20)
+        if rightSensor.value(2) > 50:
+            globalGrid.addcoord(Ev3Coordinates(0,0,"Color_white"))
             robot.speedUp(rightMotor, 20)
-
+    globalGrid.exportcsv()
         #robot.speedUp(leftMotor, 10)
         #robot.speedUp(rightMotor, 10)
