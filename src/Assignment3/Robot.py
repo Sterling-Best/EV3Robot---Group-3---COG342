c416847ce046b7ded76e00e7be9d04c51b6c7d7a
Sterling <sterling@mysix.com>
2018-03-24 22:31:12 -0400

    Robot.py - Fixed initiation of Tank class

--- src/Assignment3/Robot.py
+++ src/Assignment3/Robot.py
@@ -27,7 +27,7 @@ class Robot:
         #Initiate Motors
         self.motorLeft = ev3.LargeMotor('outA')
         self.motorRight = ev3.LargeMotor('outD')
-        self.motorPair = helper.Tank(self.motorLeft, self.motorRight)
+        self.motorPair = helper.Tank('outA', 'outD')
         # Initiate Sensors
         self.ultrasonic = ev3.UltrasonicSensor('in1')
         assert self.ultrasonic.connected
