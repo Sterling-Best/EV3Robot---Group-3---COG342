cedcb9a0781b0ba448036847b5a319160745548b
Sterling <sterling@mysix.com>
2018-03-24 23:31:54 -0400

    Robot.py - Changed Rotation Multiplier

--- src/Assignment3/Robot.py
+++ src/Assignment3/Robot.py
@@ -38,7 +38,7 @@ class Robot:
         self.LED.all_off()
         initiatldistace = round((self.ultrasonic.value()/10) / 2.54)
         estimateddistance = initiatldistace - distance
-        rotation = distance * 52
+        rotation = distance * 53
         self.motorLeft.run_to_rel_pos(speed_sp=200, position_sp=rotation)
         self.motorRight.run_to_rel_pos(speed_sp=200, position_sp=rotation)
         self.motorLeft.wait_while(self.motorLeft.STATE_RUNNING)
