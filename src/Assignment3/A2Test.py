b5aa26c37d478d13731aff29c1adb548acf15f50
Sterling <sterling@mysix.com>
2018-03-30 20:15:23 -0400

    A2Test.py

--- src/Assignment3/A2Test.py
+++ src/Assignment3/A2Test.py
@@ -44,13 +44,13 @@ def run() -> None:
 
     robot.LCD.clear()
     robot.LCD.update()
-    for x in range(60):
+    for x in range(45):
         distance = (robot.ultrasonic.value()/10) + 2.5
-        theta = robot.get_currentdegrees()
+        theta = radians(robot.get_currentdegrees())
         x = int(distance * sin(theta))
         y = int(distance * cos(theta))
         robot.globalGrid.addcoord(Ev3Coordinates(x,y,"PointOfObstruction"))
-        robot.tankrotate(6)
+        robot.tankrotate(2)
     robot.LCD.clear()
     robot.lcddrawglobal()
     robot.LCD.update()
